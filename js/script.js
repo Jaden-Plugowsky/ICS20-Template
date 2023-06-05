2bc34fa3eade7f866cecea7cb2cb01300595b4b1
Jaden Plugowsky <124389316+Jaden-Plugowsky@users.noreply.github.com>
2023-06-05 01:29:13 +0000

    added PWA compatibility

--- js/script.js
+++ js/script.js
@@ -1,13 +1,27 @@
 // Copyright (c) 2023 Jaden Plugowsky All rights reserved
 //
 // Created by: Jaden Plugowsky
-// Created on: March 2023
+// Created on: June 2023
 // This file contains the JS functions for index.html
 
 "use strict";
 
+// Checks service worker
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Template/sw.js", {
+    scope: "/ICS2O-Template/",
+  })
+}
+
 function buttonOneClicked() {
-  alert("Hello, World!");
+  //This function does XXXX
+  //Input through Textfields
+  const example = parseFloat(document.getElementById("example").value)
+
+  //Process
+  const answer = example
+
+  //Output
   document.getElementById("answer").innerHTML =
-    "The answer is: " + "insert answer here";
+    "The answer is: " + answer.toFixed(2)
 }
